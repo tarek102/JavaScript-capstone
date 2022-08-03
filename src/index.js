d8d5cb40c8eaf88cb849f509c09568176d9cb595
anasse Laarabi <anas@MacBook-Pro-de-anasse.local>
2022-08-03 12:28:40 +0100

    updating index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import _ from 'lodash';
 import './style.css';
 
+const btnComments = document.querySelectorAll('.comments');
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.closeBtn');
+
+btnComments.forEach(btn => {
+  btn.addEventListener('click', () => {
+    popup.classList.remove('visible');
+    console.log("clicked");
+  });
+});
+
+close.addEventListener('click', ()=> {
+  
+})
 
 
-document.body.appendChild(component());
+
+// document.body.appendChild(component());
