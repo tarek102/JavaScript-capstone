ac6d3ceb44d5722110341d738bd509bf7ff3322a
Tarek <83262890+tarek102@users.noreply.github.com>
2022-08-03 13:28:40 +0200

    Get data from Api and display them on homepage

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,14 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
 };
