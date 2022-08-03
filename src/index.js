1c42bd30c180d13e1b66a17116bb2999f3f7510e
anasse Laarabi <anas@MacBook-Pro-de-anasse.local>
2022-08-03 17:33:40 +0100

    updating index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
-// import _ from 'lodash';
-// import './style.css';
-// ################################
-class Api {
-  constructor (url) {
-    this.url = url;
-  }
-
-  getData = async(url) => {
-    const response = await fetch (url, {
-      method: 'GET',
-    });
-    const res = await response.json();
-    const responseArr = await res;
-    return responseArr;
-  }
-
-  getDataInvolvement = async(invUrl) => {
-    const response = await fetch(invUrl, {
-      method: 'GET',
-    });
-    const res = await response.json();
-    const invResArray = await res;
-    console.log(invResArray);
-  }
-}
-
-// ################################
-
-// import Api from './modules/api';
+import _ from 'lodash';
+import './style.css';
+import Api from './modules/api';
 
 
 
